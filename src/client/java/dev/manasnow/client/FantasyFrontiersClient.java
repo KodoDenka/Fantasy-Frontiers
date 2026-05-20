549bfda8f23ab70ad08afb4df4149a68f92070b0
KodoDenka <vinyltapefive@gmail.com>
2026-05-19 23:29:35 -0700

    Init

---
+++ src/client/java/dev/manasnow/client/FantasyFrontiersClient.java
@@ -0,0 +1,10 @@
+package dev.manasnow.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FantasyFrontiersClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/java/dev/manasnow/item/ModItems.java
@@ -0,0 +1,4 @@
+package dev.manasnow.item;
+
+public class ModItems {
+}
